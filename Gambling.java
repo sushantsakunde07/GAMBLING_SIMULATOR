b1930814214a2fcbb21ec055d4cc808bde0e52ed
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-10 04:22:06 +0530

    Added: Gambler either win or loose

--- Gambling.java
+++ Gambling.java
@@ -3,8 +3,19 @@ package Gambling;
 public class Gambling {
     public static void main(String[] args) {
         int STAKE = 100;
-        int BET =  1;
-        System.out.println("Gambler start with stake of $" + STAKE + " every day");
-        System.out.println("Gambler bet for $" + BET + " every game");
+        System.out.println("Gambler start with stake of $" + STAKE);
+        int TRIAL = (int) Math.floor(Math.random() * 2);
+        System.out.println(+TRIAL);
+        if (TRIAL == 0) {
+            System.out.println("Gambler won the game");
+            STAKE++;
+        } else {
+            System.out.println("Gambler loose the game");
+            STAKE--;
+        }
+        {
+            System.out.println("Gambler having stake after bet are of $ " + STAKE);
+        }
+
     }
 }
