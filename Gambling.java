41af1584acb12aa65e3cebb0210d13c9c8bdc801
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-10 04:11:21 +0530

    Added: Gambler having stake $100 and bet for $1

---
+++ Gambling.java
@@ -0,0 +1,10 @@
+package Gambling;
+
+public class Gambling {
+    public static void main(String[] args) {
+        int STAKE = 100;
+        int BET = 1;
+        System.out.println("Gambler start with stake of $" + STAKE + " every day");
+        System.out.println("Gambler bet for $" + BET + " every game");
+    }
+}
