af258a95e75b266fc886a937556b18f3c1bc8c2e
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-10 04:36:04 +0530

    Added: Total amount win or loose after 20 days

--- Gambling.java
+++ Gambling.java
@@ -2,19 +2,32 @@ package Gambling;
 
 public class Gambling {
     public static void main(String[] args) {
-        int STAKE =100;
-        System.out.println("Gambler start with stake of $" + STAKE);
-        int TRIAL = (int) Math.floor(Math.random() * 2);
-        System.out.println(+TRIAL);
-        if (TRIAL == 0) {
-            System.out.println("Gambler won the game");
-            STAKE++;
-        } else {
-            System.out.println("Gambler loose the game");
-            STAKE--;
+        int win = 0;
+        int loose = 0;
+        for (int i = 0; i < 20; i++) {
+            int stake = 100;
+            while (stake < 150 && stake > 50) {
+                int TRIAL = (int) Math.floor(Math.random() * 2);
+                if (TRIAL == 0) {
+                    // System.out.println("Gambler won the game");
+                    stake++;
+                } else {
+                    //  System.out.println("Gambler loose the game");
+                    stake--;
+                }
+            }
+            if (stake == 150) {
+                win++;
+            } else if (stake == 50) {
+                loose++;
+            }
+            {
+                System.out.println("Gambler having stake after bet are of $ " + stake);
+            }
         }
         {
-            System.out.println("Gambler having stake after bet are of $ " + STAKE);
+            int stakeFINAL = win * 50 - loose * 50 + 100;
+            System.out.println("Gambler having stake after 20 days $ " + stakeFINAL);
         }
 
     }
